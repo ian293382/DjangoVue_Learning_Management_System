a781cc752b38564d326106b4d04161469cdd2f76
Iannaan <moshou5571228@gmail.com>
2024-01-04 00:53:39 +0800

    Clearn up the App.vue and Home.vue add navbar,menu,content and footer

--- studynet_vue/src/App.vue
+++ studynet_vue/src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div class="nav">
+  <nav class="navbar" role="navigation" aria-label="main navigation" style="min-height: 5rem;">
+    <a href="/" class="navbar-item is-size-4">StudyNet</a>
+
+    <div class="navbar-menu" id="navbar-item">
+      <div class="navbar-start">
+        <a href="#" class="navbar-item">Home</a>
+        <a href="#" class="navbar-item">About</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a href="#" class="button is-primary"><strong>Sign up</strong></a>
+            <a href="#" class="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </nav>
+</div>
+
   <router-view/>
+
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <style lang="scss">
 @import '../node_modules/bulma';
-
-
 </style>
