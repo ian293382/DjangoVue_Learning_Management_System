23e7a41945928e82a8dc6497cdc24e923ac4ac2f
Iannaan <moshou5571228@gmail.com>
2024-01-04 15:17:36 +0800

    Convert the Navbar to a component

--- studynet_vue/src/App.vue
+++ studynet_vue/src/App.vue
@@ -1,26 +1,7 @@
 <template>
-<div class="nav">
-  <nav class="navbar is-info" role="navigation" aria-label="main navigation" style="min-height: 5rem;">
-    <a href="/" class="navbar-item is-size-4 white">StudyNet</a>
-
-    <div class="navbar-menu" id="navbar-item">
-      <div class="navbar-start">
-        <a href="/" class="navbar-item">Home</a>
-        <a href="/About" class="navbar-item">About</a>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a href="/sign-up" class="button is-primary"><strong>Sign up</strong></a>
-            <a href="/log-in" class="button is-light">Log in</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </nav>
-</div>
+  <div class="nav">
+    <Nav/>
+  </div>
 
   <router-view/>
 
@@ -28,6 +9,18 @@
     <p class="has-text-centered">Copyright (c) 2021</p>
   </footer>
 </template>
+
+<script>
+import Nav from '@/components/Nav';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  }
+}
+
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
