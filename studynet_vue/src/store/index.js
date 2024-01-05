4c2c1b4c1ca83a2ab631da8f636c0704d25a9ec1
Iannaan <moshou5571228@gmail.com>
2024-01-05 23:27:08 +0800

    Set up vuex store with user

--- studynet_vue/src/store/index.js
+++ studynet_vue/src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {
+      token: '',
+      isAuthenticated: falsex``
+    }
   },
   getters: {
   },
   mutations: {
+    initializeStore(state) {
+      if (localStoreage.getItem('token')) {
+        state.user.token = localStoreage.getItem('token')
+        state.user.isAuthenticated = true
+      } else {
+        state.user.token = ''
+        state.user.isAuthenticated = false
+      }
+    },
+    setToken(state, token) {
+      state.user.token = token 
+      state.user.isAuthenticated = true
+    }
   },
   actions: {
   },
