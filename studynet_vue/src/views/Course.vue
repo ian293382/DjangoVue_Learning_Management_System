2bc6a497fec962e31836943817d0a39bb2051ba9
Iannaan <moshou5571228@gmail.com>
2024-01-07 16:43:14 +0800

    create Course.vue build router courses/:slug

---
+++ studynet_vue/src/views/Course.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="courses">
+      <div class="hero is-info is-medium">
+        <div class="hero-body has-text-centered">
+          <h1 class="title">Course</h1>
+        </div>
+      </div>
+      <section class="section">
+        <div class="container">
+         <div class="columns">
+
+         </div>
+        </div>
+      </section>
+    </div>
+  </template>
