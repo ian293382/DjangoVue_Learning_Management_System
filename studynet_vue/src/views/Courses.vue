c1e9df3077710a3792ec2ca489c363baccc2fd6c
Iannaan <moshou5571228@gmail.com>
2024-01-08 02:21:22 +0800

    Convert the course list object to a component

--- studynet_vue/src/views/Courses.vue
+++ studynet_vue/src/views/Courses.vue
@@ -27,29 +27,8 @@
                      v-for="course in courses"
                      v-bind:key="courses.id"
                 >
-
-                  <div class="card">
-                    <div class="card-image">
-                      <figure class="image is-4by3">
-                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                      </figure>
-                    </div>
-  
-                    <div class="card-content">
-                      <div class="media">
-                        <div class="media-content">
-                          <p class="is-size-5">{{ course.title }}</p>
-                        </div>
-                      </div>
-  
-                      <div class="content">
-                        <p>{{ course.short_description }}</p>
-                      <router-link :to="{name: 'Course', params: {slug: course.slug}}">More</router-link>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                    <CourseItem :course="course"/>
+               </div>
          
   
                 <div class="column is-12">
@@ -80,25 +59,28 @@
   </template>
   
 <script>
+import CourseItem from '@/components/CourseItem.vue';
 import axios from 'axios';
 
 export default {
     data() {
         return {
             courses: []
-        }
+        };
     },
-
+    components: {
+        CourseItem 
+    },
     mounted() {
-        console.log('mounted')
+        console.log('mounted');
         axios
             .get('/courses/')
             .then(response => {
-                console.log(response.data)
-              
-                this.courses = response.data
-            })
-    }
+            console.log(response.data);
+            this.courses = response.data;
+        });
+    },
+    
 }
 
 </script>
