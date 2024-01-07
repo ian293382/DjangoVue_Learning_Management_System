7809d3a7a5ff54ac5a8cee285b170b398e40e189
Iannaan <moshou5571228@gmail.com>
2024-01-07 14:09:02 +0800

    Django  Add Course app Build Category and Course database

---
+++ studynet_django/course/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add= True)
+
+class Course(models.Model):
+    categories = models.ManyToManyField(Category)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.TextField(blank=True, null=True)
+    long_description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add= True)
