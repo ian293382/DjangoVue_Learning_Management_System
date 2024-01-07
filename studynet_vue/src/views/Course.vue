e1b0041e88eefa7c72b5bad4d183971d2aca87ff
Iannaan <moshou5571228@gmail.com>
2024-01-07 16:47:47 +0800

    build Course content and side bar

--- studynet_vue/src/views/Course.vue
+++ studynet_vue/src/views/Course.vue
@@ -2,13 +2,28 @@
     <div class="courses">
       <div class="hero is-info is-medium">
         <div class="hero-body has-text-centered">
-          <h1 class="title">Course</h1>
+          <h1 class="title">The title of the Course</h1>
         </div>
       </div>
       <section class="section">
         <div class="container">
-         <div class="columns">
+         <div class="columns content">
+            <div class="column is-2">
+                <h2>Table of contents</h2>
 
+                <ul>
+                    <li><a href="#">Introduction</a></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">Part I</a></li>
+                    <li><a href="#">Part II</a></li>
+                    <li><a href="#">Summary</a></li>
+                </ul>
+            </div>
+            <div class="column is-10">
+                <h2>Introduction</h2>
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel magni tempore quisquam adipisci, assumenda impedit fugiat officia veritatis dignissimos voluptatum nemo nesciunt quas quis dolores dicta, commodi placeat ab reprehenderit.</p>
+            </div>
          </div>
         </div>
       </section>
