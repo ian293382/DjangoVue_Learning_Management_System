fedd5781ca86f4438ce67b0b3ff4b409ea2d7142
Iannaan <moshou5571228@gmail.com>
2024-01-07 13:25:28 +0800

    implement column system for content block image is-4by3

--- studynet_vue/src/views/Courses.vue
+++ studynet_vue/src/views/Courses.vue
@@ -3,33 +3,125 @@
       <div class="hero is-info is-medium">
         <div class="hero-body has-text-centered">
           <h1 class="title">Course</h1>
-  
-  
         </div>
       </div>
       <section class="section">
         <div class="container">
-            <div class="columns">
-                <div class="column is-2">
-                    <aside class="menu">
-                        <p class="menu-label">Categories</p>
-
-                        <ul class="menu-list">
-                            <li><a href="" class="is-active">All courses</a></li>
-                            <li><a href="">Programming</a></li>
-                            <li><a href="">Design</a></li>
-                            <li><a href="">UX</a></li>
-                        </ul>
-                    </aside>
-                </div>
-
-                <div class="columnㄕ is-10">
-                    Content
-                </div>
+          <div class="columns">
+            <div class="column is-2">
+              <aside class="menu">
+                <p class="menu-label">Categories</p>
+                <ul class="menu-list">
+                  <li><a href="" class="is-active">All courses</a></li>
+                  <li><a href="">Programming</a></li>
+                  <li><a href="">Design</a></li>
+                  <li><a href="">UX</a></li>
+                </ul>
+              </aside>
             </div>
+  
+            <div class="column is-10">
+              <div class="columns is-multiline">
+                <!-- 排列圖片從這個block開始 -->
+                <div class="column is-4">
+                  <div class="card">
+                    <div class="card-image">
+                      <figure class="image is-4by3">
+                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                      </figure>
+                    </div>
+  
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-content">
+                          <p class="is-size-5">Build a social network</p>
+                        </div>
+                      </div>
+  
+                      <div class="content">
+                        <p>Learn the basics bla bla bla...</p>
+                        <a href="#">More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="column is-4">
+                  <div class="card">
+                    <div class="card-image">
+                      <figure class="image is-4by3">
+                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                      </figure>
+                    </div>
+  
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-content">
+                          <p class="is-size-5">Build a social network</p>
+                        </div>
+                      </div>
+  
+                      <div class="content">
+                        <p>Learn the basics bla bla bla...</p>
+                        <a href="#">More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="column is-4">
+                  <div class="card">
+                    <div class="card-image">
+                      <figure class="image is-4by3">
+                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                      </figure>
+                    </div>
+  
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-content">
+                          <p class="is-size-5">Build a social network</p>
+                        </div>
+                      </div>
+  
+                      <div class="content">
+                        <p>Learn the basics bla bla bla...</p>
+                        <a href="#">More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="column is-4">
+                  <div class="card">
+                    <div class="card-image">
+                      <figure class="image is-4by3">
+                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                      </figure>
+                    </div>
+  
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-content">
+                          <p class="is-size-5">Build a social network</p>
+                        </div>
+                      </div>
+  
+                      <div class="content">
+                        <p>Learn the basics bla bla bla...</p>
+                        <a href="#">More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                <!-- Repeat the above card structure for additional cards -->
+  
+              </div>
+            </div>
+          </div>
         </div>
       </section>
-  
     </div>
   </template>
   
