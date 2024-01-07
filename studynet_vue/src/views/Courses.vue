71d75657b7e66ea681d9fc03ba0ad34c5b5c0e7f
Iannaan <moshou5571228@gmail.com>
2024-01-07 13:53:08 +0800

    Build pagination page

--- studynet_vue/src/views/Courses.vue
+++ studynet_vue/src/views/Courses.vue
@@ -115,7 +115,24 @@
                   </div>
                 </div>
   
-                <!-- Repeat the above card structure for additional cards -->
+                <div class="column is-12">
+                    <nav class="pagination">
+                        <a href="" class="pagination-previous">Previous</a>
+                        <a href="" class="pagination-next">Next</a>
+
+                        <ul class="pagination-list">
+                            <li>
+                                <a href="" class="pagination-link is-current">1</a>
+                            </li>
+                            <li>
+                                <a href="" class="pagination-link">2</a>
+                            </li>
+                            <li>
+                                <a href="" class="pagination-link">3</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
   
               </div>
             </div>
