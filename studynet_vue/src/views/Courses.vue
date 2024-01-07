316037de4176ec92d641bb687fd1e619453956d2
Iannaan <moshou5571228@gmail.com>
2024-01-07 12:24:22 +0800

    Create Courses.vue

---
+++ studynet_vue/src/views/Courses.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="courses">
+      <div class="hero is-info is-medium">
+        <div class="hero-body has-text-centered">
+          <h1 class="title">Course</h1>
+  
+  
+        </div>
+      </div>
+  
+ >
+  
+    </div>
+  </template>
+  
