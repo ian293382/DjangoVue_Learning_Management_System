678ecfcc7191119875ce5b844681f31e3fb0eb41
Iannaan <moshou5571228@gmail.com>
2024-01-08 15:25:44 +0800

    implement image in course to thumbnails to course

--- studynet_django/course/urls.py
+++ studynet_django/course/urls.py
@@ -1,12 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from course import views
-
+ # 特例寫在上方 因為slug也會包含到get_frontpage_courses
 urlpatterns = [
     path('', views.get_courses),
-    # 特例寫在上方 因為slug也會包含到get_frontpage_courses
+    
     path('get_frontpage_courses/', views.get_frontpage_courses), 
     path('<slug:slug>/', views.get_course),
     path('<slug:course_slug>/<slug:lesson_slug>/', views.add_comment),
     path('<slug:course_slug>/<slug:lesson_slug>/get-comments/', views.get_comments),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- studynet_vue/src/components/CourseItem.vue
+++ studynet_vue/src/components/CourseItem.vue
@@ -2,7 +2,7 @@
        <div class="card">
             <div class="card-image">
                 <figure class="image is-4by3">
-                <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                <img :src="course.get_image" alt="Placeholder image">
                 </figure>
             </div>
 
