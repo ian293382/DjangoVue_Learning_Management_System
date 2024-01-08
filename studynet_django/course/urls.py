da4b60583f391d84b54c73c2ff6918d843a9f054
Iannaan <moshou5571228@gmail.com>
2024-01-08 14:48:20 +0800

    Implement newest courses on the front page

--- studynet_django/course/urls.py
+++ studynet_django/course/urls.py
@@ -4,6 +4,8 @@ from course import views
 
 urlpatterns = [
     path('', views.get_courses),
+    # 特例寫在上方 因為slug也會包含到get_frontpage_courses
+    path('get_frontpage_courses/', views.get_frontpage_courses), 
     path('<slug:slug>/', views.get_course),
     path('<slug:course_slug>/<slug:lesson_slug>/', views.add_comment),
     path('<slug:course_slug>/<slug:lesson_slug>/get-comments/', views.get_comments),
