f5986823b48ed79564912e98dba8731e2cfdbbab
Iannaan <moshou5571228@gmail.com>
2024-01-13 18:19:58 +0800

    implement Click to get started in Home page

--- studynet_vue/src/views/HomeView.vue
+++ studynet_vue/src/views/HomeView.vue
@@ -40,7 +40,7 @@
           </div>
 
           <div class="column is-12 has-text-centered">
-            <a href="#" class="button is-info is-size-3 mt-6 mb-6">Click to get started</a>
+            <a href="/sign-up" class="button is-info is-size-3 mt-6 mb-6">Click to get started</a>
           </div>
 
           <hr>
@@ -79,7 +79,7 @@ export default {
     console.log('mounted')
 
     document.title = 'Welcome | StudyNet'
-    
+
     axios
           .get('/courses/get_frontpage_courses/')
           .then(response => {
