18ae996c5e844a892deb67e2936915c10df4cd52
Iannaan <moshou5571228@gmail.com>
2024-01-13 16:54:51 +0800

    register Comment in admin section

--- studynet_django/course/admin.py
+++ studynet_django/course/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
-from .models import Category, Course, Lesson
+from .models import Category, Course, Lesson, Comment
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title', 'course', 'status', 'lesson_type']
     list_filter = ['status', 'lesson_type']
+    search_fields = ['title', 'short_description', 'long_description']
 
 admin.site.register(Category)
 
 admin.site.register(Course)
 
 admin.site.register(Lesson, LessonAdmin)
+
+admin.site.register(Comment)
