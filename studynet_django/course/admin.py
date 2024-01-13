39be4f8ab7d2d6f9cb3f80c10d6439b79eabf15b
Iannaan <moshou5571228@gmail.com>
2024-01-13 17:02:12 +0800

    Setting LessonCommentInline improved display with courses using raw_id_fields in admin section

--- studynet_django/course/admin.py
+++ studynet_django/course/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 
 from .models import Category, Course, Lesson, Comment
 
+
+class LessonCommentInline(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['lesson']
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title', 'course', 'status', 'lesson_type']
     list_filter = ['status', 'lesson_type']
     search_fields = ['title', 'short_description', 'long_description']
+    inlines = [LessonCommentInline]
 
 admin.site.register(Category)
 
