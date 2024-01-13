c3cd0d0f4d2ef26b2bdb40be39f34fbfa3106cf9
Iannaan <moshou5571228@gmail.com>
2024-01-13 18:41:24 +0800

    Prevent loading data from course (when not signed in )

--- studynet_django/course/views.py
+++ studynet_django/course/views.py
@@ -39,19 +39,28 @@ def get_frontpage_courses(request):
 
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_course(request, slug):
     course = Course.objects.get(slug=slug)
     course_serializer = CourseDetailSerializer(course)
     lesson_serializer = LessonsListSerializer(course.lessons.all(), many=True)
 
+    if request.user.is_authenticated:
+        course_data = course_serializer.data
+    else:
+        course_data = {}
+
     data = {
-        'course': course_serializer.data,
+        'course': course_data,
         'lessons': lesson_serializer.data
     }
 
     return Response(data)
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_comments(request, course_slug, lesson_slug):
     lesson = Lesson.objects.get(slug=lesson_slug)
     serializer = CommentSerializer(lesson.comments.all(), many=True)
