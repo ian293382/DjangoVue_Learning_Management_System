a228f7bc179fcec2419c6a95f96bf8ebc7735a4e
Iannaan <moshou5571228@gmail.com>
2024-01-14 22:00:50 +0800

    Create activity database

--- studynet_django/activity/models.py
+++ studynet_django/activity/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from course.models import Course, Lesson
+
+class Activity(models.Model):
+    STARTED = 'started'
+    DONE = 'done'
+
+    STATUS_CHOICES = (
+        (STARTED, 'Started'),
+        (DONE, 'Done')
+    )
+
+    couser = models.ForeignKey(Course, related_name='activities', on_delete=models.CASCADE)
+    Losson = models.ForeignKey(Lesson, related_name='activities', on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=STARTED)
+    created_by = models.ForeignKey(User, related_name='activities', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
