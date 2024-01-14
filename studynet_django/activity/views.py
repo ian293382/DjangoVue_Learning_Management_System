0f61734f702bc4465814d39a19561f72767c202f
Iannaan <moshou5571228@gmail.com>
2024-01-14 22:11:49 +0800

    Add activity course view

--- studynet_django/activity/views.py
+++ studynet_django/activity/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Activity
+
+from course.serializers import CourseListSerializer
+
+@api_view(['GET'])
+def get_active_courses(request):
+    courses = []
+
+    for activity in request.user.activities.all():
+        if activity.status == activity.STARTED and activity.course not in courses:
+            courses.append(activity.course)
+    
+    serializer = CourseListSerializer(courses, many=True)
+
+    return Response(serializer.data)
