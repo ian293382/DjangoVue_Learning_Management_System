36485808dbc1b5548b4fe1e773ebae4f40bb636a
Iannaan <moshou5571228@gmail.com>
2024-01-15 02:17:00 +0800

    Create CreateCouse.vue

---
+++ studynet_vue/src/views/dashboard/CreateCouse.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="about">
+        <div class="hero is-info is-medium">
+        <div class="hero-body has-text-centered">
+            <h1 class="title">Crete course</h1>
+        </div>
+      </div>
+    
+        <section class="section">
+        
+        </section>>
+    </div>  
+</template>
