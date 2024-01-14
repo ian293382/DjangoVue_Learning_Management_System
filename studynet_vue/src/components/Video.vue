45bbc31e3f74272a3e050cba32385eb416c54a84
Iannaan <moshou5571228@gmail.com>
2024-01-14 21:27:25 +0800

    implement Embed Youtube video

---
+++ studynet_vue/src/components/Video.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="video-container">
+    <iframe 
+            :src="'https:/www.youtube.com/embed/' + video_id"
+            width="100%"
+            height="500"  
+            frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+            allowfullscreen>
+    </iframe>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['video_id']
+}
+</script>
